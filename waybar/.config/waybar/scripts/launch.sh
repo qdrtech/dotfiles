34f5f1ac56d5a2c10b39f5055f02f5e2541956c9
qdrtech <qdr.dev@gmail.com>
2025-03-07 22:12:08 -0800

    - add waybar .config

---
+++ waybar/.config/waybar/scripts/launch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# by qdr_tech (2024)
+
+# Quit running instances of waybar
+killall waybar
+
+# Loading the configuration based on the username
+if [[ $USER = "qdrtech" ]]; then
+  waybar -c ~/.config/waybar/config &
+else
+  waybar &
+fi
+
